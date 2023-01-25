8a306afc2e5d3858b8ec911b4bb61fb771d2bbfe
burak.kocak <burakkocak@outlook.com.tr>
2023-01-26 00:49:00 +0300

    adding some more tests and more refactoring

--- src/com/sportradar/utils/Constants.java
+++ src/com/sportradar/utils/Constants.java
@@ -1,6 +1,9 @@
 package com.sportradar.utils;
 
 public class Constants {
+    /**
+     * ErrorCode list
+     */
     public static final String NOT_FOUND = "Item not found!";
     public static final String MATCH_CANNOT_BE_RECREATED = "Cannot re-create an already started match!";
     public static final String MATCH_CANNOT_BE_UPDATED = "Cannot update score of a finished match!";
